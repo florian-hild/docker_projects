acbd58806493acca4b9c9eae4b9f2a8907272166
Florian Hild <florian.hild@gmail.com>
2022-12-21 22:12:13 +0100

    add: docker compose file for homeassistant01 :tada:

---
+++ home_assistant/start_homeassistant01.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#-------------------------------------------------------------------------------
+# Author     : Florian Hild
+# Created    : 21-12-2022
+# Description:
+#-------------------------------------------------------------------------------
+
+export LANG=C
+declare -r __SCRIPT_VERSION__='1.0'
+declare -r docker_compose_file='docker-compose_homeassistant01.yml'
+
+docker-compose -f ${docker_compose_file} up \
+  --force-recreate \
+  --build \
+  --detach
+
